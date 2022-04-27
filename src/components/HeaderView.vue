0c34ef0eea0d0da972aa77cd96f90661f3bd8e02
ldsleticia <lds.leticia.dos.santos@gmail.com>
2022-04-27 14:26:03 -0300

    site wdev

---
+++ src/components/HeaderView.vue
@@ -0,0 +1,153 @@
+<template>
+  <header>
+    <nav class="container">
+      <a href="/">
+        <img
+          alt="WDEV"
+          id="logo"
+          src="https://raw.githubusercontent.com/william-costa/wdev-mock-site-resources/master/assets/images/wdev.svg"
+        />
+      </a>
+
+      <img
+        @click="openMenu"
+        src="https://raw.githubusercontent.com/william-costa/wdev-mock-site-resources/master/assets/images/menu.svg"
+        alt="Abrir menu"
+        id="menu-button"
+      />
+
+      <div @click="closeMenu" id="menu-overlay" v-if="menuActive"></div>
+
+      <div id="menu-itens" :class="{ active: menuActive }">
+        <img
+          alt="WDEV"
+          id="menu-logo"
+          src="https://raw.githubusercontent.com/william-costa/wdev-mock-site-resources/master/assets/images/wdev.svg"
+        />
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/videos">Videos</a></li>
+          <li><a href="/sobre">Sobre</a></li>
+          <li><a href="/contatos">Contatos</a></li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      menuActive: false,
+    };
+  },
+  methods: {
+    openMenu() {
+      this.menuActive = true;
+    },
+
+    closeMenu() {
+      this.menuActive = false;
+    },
+  },
+};
+</script>
+
+<style scoped>
+header {
+  background-color: var(--color-background-nav);
+  width: 100%;
+  height: 60px;
+  display: flex;
+  justify-content: center;
+  align-content: center;
+}
+
+nav {
+  display: flex;
+  justify-content: space-between;
+  height: 60px;
+  align-items: center;
+}
+
+#logo {
+  width: 130px;
+}
+
+#menu-button {
+  width: 30px;
+}
+
+#menu-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 40%;
+  height: 100vh;
+  background-color: black;
+  opacity: 0.8;
+}
+
+#menu-logo {
+  width: 110px;
+  margin-top: 30px;
+  margin-bottom: 10px;
+}
+
+#menu-itens {
+  position: fixed;
+  top: 0;
+  right: 0;
+  background-color: var(--color-background-nav);
+  width: 60%;
+  height: 100vh;
+  display: none;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+}
+
+#menu-itens.active {
+  display: flex;
+}
+
+ul {
+  list-style: none;
+  text-align: center;
+}
+
+ul li {
+  margin: 20px 0px;
+}
+
+ul li a {
+  color: var(--color-text-light);
+}
+
+@media (min-width: 700px){
+    #menu-button,
+    #menu-logo,
+    #menu-overlay {
+        display: none;
+    }
+
+    #menu-itens {
+        display: flex;
+        position: static;
+        height: 60px;
+        width: auto;
+    }
+
+    ul {
+        display: flex;
+        height: 60px;
+        align-items: center;
+    }
+
+    ul li {
+        margin: 0;
+        margin-left: 20px;
+    }
+}
+</style>
---
+++ src/pages/HomeView.vue
@@ -0,0 +1,49 @@
+<template>
+  <main>
+    <div class="text-top">
+      Vídeos novos toda <strong>quinta</strong> às <strong>20h</strong>
+    </div>
+    <a href="google.com.br" target="blank">
+      <img
+        src="https://raw.githubusercontent.com/william-costa/wdev-mock-site-resources/master/assets/images/subscribe.svg"
+        alt="inscreva-se"
+        id="subscribe"
+      />
+    </a>
+    <div class="text-botton">google.com.br</div>
+  </main>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+main {
+  background-color: var(--color-background-home);
+  justify-content: center;
+  align-items: center;
+}
+
+.text-top {
+  color: var(--color-text-light);
+  font-size: 22px;
+  margin-bottom: 20px;
+}
+
+#subscribe {
+  width: 340px;
+}
+
+.text-botton {
+  color: var(--color-text-light);
+  font-size: 18px;
+  margin-top: 20px;
+}
+
+@media (min-width: 700px) {
+  #subscribe {
+    width: 370px;
+  }
+}
+</style>
